cff8b7f25c658518845ce5bfa428db7c436dd755
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-30 23:39:28 +0200

    -

--- code mit java/index.java
+++ code mit java/index.java
@@ -1,5 +1,10 @@
 public class index {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+        
     }
 }
