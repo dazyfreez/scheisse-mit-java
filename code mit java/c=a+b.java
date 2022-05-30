2d7d44a00c44385a2b217bb9a013651860ba012e
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-30 23:40:21 +0200

    -

---
+++ code mit java/c=a+b.java
@@ -0,0 +1,10 @@
+public class  {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+        
+    }
+}
